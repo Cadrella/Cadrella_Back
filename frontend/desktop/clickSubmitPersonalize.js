ac9f06c7ea1f8c921ba3a4be4e30eec014f70e50
Adel Manseur <ad.manseur@univ-alger.dz>
2025-07-06 13:49:07 +0100

    Update clickSubmitPersonalize.js

--- frontend/desktop/clickSubmitPersonalize.js
+++ frontend/desktop/clickSubmitPersonalize.js
@@ -24,7 +24,7 @@ const clickOnSubmitPersonalize = async (event) => {
             formObject.image_input = base64Image;
 
             // Now send it
-            fetch('http://localhost:3000/personalize', {
+            fetch('https://cadrella-back.onrender.com//personalize', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json'
